182e27d6935951571cf312493a5bdd6d916c056e
Nick Manos <nmanos@eastonrobotics.org>
2025-01-29 17:06:52 -0500

    Update USB HID descriptor to include all required buttons and LEDs

--- boot.py
+++ boot.py
@@ -21,24 +21,48 @@ else:
 
 # fmt: off
 GAMEPAD_REPORT_DESCRIPTOR = bytes((
-    0x05, 0x01,                    # USAGE_PAGE (Generic Desktop)
-    0x09, 0x05,                    # USAGE (Game Pad)
-    0xA1, 0x01,                    # COLLECTION (Application)
-    0xA1, 0x00,                    #   COLLECTION (Physical)
-    0x85, 0x01,                    #     REPORT_ID (1)
-    0x05, 0x09,                    #     USAGE_PAGE (Button)
-    0x1B, 0x01, 0x00, 0x09, 0x00,  #     USAGE_MINIMUM (Button:Button 1)
-    0x2B, 0x0A, 0x00, 0x09, 0x00,  #     USAGE_MAXIMUM (Button:Button 10)
-    0x15, 0x00,                    #     LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    #     LOGICAL_MAXIMUM (1)
-    0x95, 0x0A,                    #     REPORT_COUNT (10)
-    0x75, 0x01,                    #     REPORT_SIZE (1)
-    0x81, 0x02,                    #     INPUT (Data,Var,Abs)
-    0x75, 0x06,                    #     REPORT_SIZE (6)
-    0x95, 0x01,                    #     REPORT_COUNT (1)
-    0x81, 0x01,                    #     INPUT (Cnst,Ary,Abs)
-    0xC0,                          #   END_COLLECTION
-    0xC0,                          # END_COLLECTION
+    0x05, 0x01,         # USAGE_PAGE (Generic Desktop)
+    0x09, 0x05,         # USAGE (Game Pad)
+    0xa1, 0x01,         # COLLECTION (Application)
+    0xa1, 0x00,         #   COLLECTION (Physical)
+    0x85, 0x01,         #     REPORT_ID (1)
+    0x05, 0x09,         #     USAGE_PAGE (Button)
+    0x19, 0x01,         #     USAGE_MINIMUM (Button 1)
+    0x29, 0x0c,         #     USAGE_MAXIMUM (Button 12)
+    0x15, 0x00,         #     LOGICAL_MINIMUM (0)
+    0x25, 0x01,         #     LOGICAL_MAXIMUM (1)
+    0x95, 0x0c,         #     REPORT_COUNT (12)
+    0x75, 0x01,         #     REPORT_SIZE (1)
+    0x81, 0x02,         #     INPUT (Data,Var,Abs)
+    0x95, 0x01,         #     REPORT_COUNT (1)
+    0x75, 0x04,         #     REPORT_SIZE (4)
+    0x81, 0x03,         #     INPUT (Cnst,Var,Abs)
+    0x05, 0x01,         #     USAGE_PAGE (Generic Desktop)
+    0x09, 0x31,         #     USAGE (Y)
+    0x15, 0x80,         #     LOGICAL_MINIMUM (-128)
+    0x25, 0x7f,         #     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,         #     REPORT_SIZE (8)
+    0x95, 0x02,         #     REPORT_COUNT (2)
+    0x81, 0x02,         #     INPUT (Data,Var,Abs)
+    0x05, 0x01,         #     USAGE_PAGE (Generic Desktop)
+    0x09, 0x37,         #     USAGE (Dial)
+    0x15, 0x80,         #     LOGICAL_MINIMUM (-128)
+    0x25, 0x7f,         #     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,         #     REPORT_SIZE (8)
+    0x95, 0x01,         #     REPORT_COUNT (1)
+    0x81, 0x06,         #     INPUT (Data,Var,Rel)
+    0xc0,               #     END_COLLECTION
+    0xa1, 0x01,         #   COLLECTION (Application)
+    0x85, 0x02,         #     REPORT_ID (2)
+    0x05, 0x08,         #     USAGE_PAGE (LEDs)
+    0x09, 0x4b,         #     USAGE (Generic Indicator)
+    0x15, 0x00,         #     LOGICAL_MINIMUM (0)
+    0x25, 0x02,         #     LOGICAL_MAXIMUM (2)
+    0x75, 0x02,         #     REPORT_SIZE (2)
+    0x95, 0x08,         #     REPORT_COUNT (8)
+    0x91, 0x02,         #     OUTPUT (Data,Var,Abs)
+    0xc0,               #     END_COLLECTION
+    0xc0,               # END_COLLECTION
 ))
 # fmt: on
 
@@ -46,9 +70,9 @@ hid_gamepad = usb_hid.Device(
     report_descriptor=GAMEPAD_REPORT_DESCRIPTOR,
     usage_page=0x01,
     usage=0x05,
-    report_ids=(1,),
-    in_report_lengths=(2,),
-    out_report_lengths=(0,),
+    report_ids=(1,2),
+    in_report_lengths=(5,0),
+    out_report_lengths=(0,2),
 )
 
 usb_hid.enable((hid_gamepad,))
---
+++ hid/tigerpad.h
@@ -0,0 +1,48 @@
+// Z:\home\nmanos\Documents\Code\ITRT\tigerpad\tigerpad\hid\tigerpad.h
+
+
+char ReportDescriptor[81] = {
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x05,                    // USAGE (Game Pad)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x85, 0x01,                    //     REPORT_ID (1)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x0c,                    //     USAGE_MAXIMUM (Button 12)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x95, 0x0c,                    //     REPORT_COUNT (12)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x75, 0x04,                    //     REPORT_SIZE (4)
+    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x15, 0x80,                    //     LOGICAL_MINIMUM (-128)
+    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x37,                    //     USAGE (Dial)
+    0x15, 0x80,                    //     LOGICAL_MINIMUM (-128)
+    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
+    0xc0,                          //     END_COLLECTION
+    0xa1, 0x01,                    //   COLLECTION (Application)
+    0x85, 0x02,                    //     REPORT_ID (2)
+    0x05, 0x08,                    //     USAGE_PAGE (LEDs)
+    0x09, 0x4b,                    //     USAGE (Generic Indicator)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x02,                    //     LOGICAL_MAXIMUM (2)
+    0x75, 0x02,                    //     REPORT_SIZE (2)
+    0x95, 0x08,                    //     REPORT_COUNT (8)
+    0x91, 0x02,                    //     OUTPUT (Data,Var,Abs)
+    0xc0,                          //     END_COLLECTION
+    0xc0                           // END_COLLECTION
+};
+
