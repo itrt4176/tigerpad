045f95e2ebfee1644795517ab35b355523838fcc
Nick Manos <nmanos@eastonrobotics.org>
2025-01-29 18:45:03 -0500

    Replace two buttons with a joystick axis

--- hid/tigerpad.h
+++ hid/tigerpad.h
@@ -9,21 +9,21 @@ char ReportDescriptor[81] = {
     0x85, 0x01,                    //     REPORT_ID (1)
     0x05, 0x09,                    //     USAGE_PAGE (Button)
     0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-    0x29, 0x0c,                    //     USAGE_MAXIMUM (Button 12)
+    0x29, 0x0a,                    //     USAGE_MAXIMUM (Button 10)
     0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
     0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-    0x95, 0x0c,                    //     REPORT_COUNT (12)
+    0x95, 0x0a,                    //     REPORT_COUNT (10)
     0x75, 0x01,                    //     REPORT_SIZE (1)
     0x81, 0x02,                    //     INPUT (Data,Var,Abs)
     0x95, 0x01,                    //     REPORT_COUNT (1)
-    0x75, 0x04,                    //     REPORT_SIZE (4)
+    0x75, 0x06,                    //     REPORT_SIZE (6)
     0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
     0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
     0x09, 0x31,                    //     USAGE (Y)
     0x15, 0x80,                    //     LOGICAL_MINIMUM (-128)
     0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
     0x75, 0x08,                    //     REPORT_SIZE (8)
-    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
     0x81, 0x02,                    //     INPUT (Data,Var,Abs)
     0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
     0x09, 0x37,                    //     USAGE (Dial)
@@ -32,7 +32,7 @@ char ReportDescriptor[81] = {
     0x75, 0x08,                    //     REPORT_SIZE (8)
     0x95, 0x01,                    //     REPORT_COUNT (1)
     0x81, 0x06,                    //     INPUT (Data,Var,Rel)
-    0xc0,                          //     END_COLLECTION
+    0xc0,                          //   END_COLLECTION
     0xa1, 0x01,                    //   COLLECTION (Application)
     0x85, 0x02,                    //     REPORT_ID (2)
     0x05, 0x08,                    //     USAGE_PAGE (LEDs)
@@ -42,7 +42,7 @@ char ReportDescriptor[81] = {
     0x75, 0x02,                    //     REPORT_SIZE (2)
     0x95, 0x08,                    //     REPORT_COUNT (8)
     0x91, 0x02,                    //     OUTPUT (Data,Var,Abs)
-    0xc0,                          //     END_COLLECTION
+    0xc0,                          //   END_COLLECTION
     0xc0                           // END_COLLECTION
 };
 
