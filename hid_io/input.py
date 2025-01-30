763ca08af5f942effc94ea4ac1fb2ab11bc5546f
Nick Manos <nmanos@eastonrobotics.org>
2025-01-30 00:27:43 -0500

    Add HID input classes for digital, analog, and rotary encoder inputs

---
+++ hid_io/input.py
@@ -0,0 +1,57 @@
+from microcontroller import Pin
+from digitalio import DigitalInOut, Direction, Pull
+from analogio import AnalogIn
+from utils import map_val, clamp
+from rotaryio import IncrementalEncoder
+
+class HIDInputBase:
+    def __init__(self, hid_id: int) -> None:
+        self._id = hid_id
+    
+    @property
+    def id(self) -> int:
+        return self._id
+    
+    @property
+    def state(self):
+        raise NotImplementedError("This is a base class which should not be used directly.")
+
+    def __lt__(self, other: 'HIDInputBase') -> bool:
+        return self.id < other.id
+
+
+class HIDDigitalInput(HIDInputBase):
+    def __init__(self, pin: Pin, hid_id: int) -> None:
+        super().__init__(hid_id)
+        self._dio = DigitalInOut(pin)
+        self._dio.direction = Direction.INPUT
+        self._dio.pull = Pull.DOWN
+    
+    @property
+    def state(self) -> bool:
+        return self._dio.value
+
+
+class HIDAnalogInput(HIDInputBase):
+    def __init__(self, pin: Pin, hid_id: int) -> None:
+        super().__init__(hid_id)
+        self._ain = AnalogIn(pin)
+    
+    @property
+    def state(self) -> int:
+        return map_val(self._ain.value, 0, 65535, -128, 127)
+
+
+class HIDRotaryEncoder(HIDInputBase):
+    def __init__(self, pin_a: Pin, pin_b: Pin, hid_id: int) -> None:
+        super().__init__(hid_id)
+        self._enc = IncrementalEncoder(pin_a, pin_b)
+        self._last_pos = 0
+    
+    @property
+    def state(self) -> int:
+        pos = self._enc.position
+        delta = pos - self._last_pos
+        self._last_pos = pos
+
+        return clamp(delta * 10, -1000, 1000)
---
+++ utils.py
@@ -0,0 +1,5 @@
+def map_val(val: int, in_min: int, in_max: int, out_min: int, out_max: int) -> int:
+    return (val - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+def clamp(val: int, low: int, high: int) -> int:
+    return min(max(low, val), high)
