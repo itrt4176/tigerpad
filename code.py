802d8ea932dc53f038754c46d6e9c9c6132d1be0
Nick Manos <nmanos@eastonrobotics.org>
2025-01-30 16:27:49 -0500

    Add LED output functionality

--- code.py
+++ code.py
@@ -1,11 +1,12 @@
 import struct
 
 import board
+from hid_io.output import HIDLEDOutput
 import usb_hid
 from adafruit_hid import find_device
 from supervisor import ticks_ms
 
-from constants import INPUT_REPORT_ID
+from constants import INPUT_REPORT_ID, OUTPUT_REPORT_ID
 from hid_io.input import HIDAnalogInput, HIDDigitalInput
 
 digital_in_pins = (
@@ -31,6 +32,10 @@ analog_hids = [
     HIDAnalogInput(pin, hid_id) for pin, hid_id in zip(analog_in_pins, analog_in_ids)
 ]
 
+led_out_pins = (board.GP1, board.GP3, board.GP5, board.GP7, board.GP9, board.GP11, board.GP13, board.GP15)
+led_out_ids = range(1, 9)
+led_hids = [HIDLEDOutput(pin, hid_id) for pin, hid_id in zip(led_out_pins, led_out_ids)]
+
 device: usb_hid.Device = find_device(usb_hid.devices, usage_page=0x01, usage=0x05)  # type: ignore
 
 last_run = ticks_ms()
@@ -53,3 +58,13 @@ while True:
 
         device.send_report(report, INPUT_REPORT_ID)
         last_run = ticks_ms()
+    
+    output_report = device.get_last_received_report(OUTPUT_REPORT_ID)
+    if output_report:
+        led_bits: int = struct.unpack_from('<H', output_report)[0]
+        for led in led_hids:
+            led.state = (led_bits >> (2 * (led.id - 1))) & 0b11 # type: ignore
+    
+    for led in led_hids:
+        led.run()
+
