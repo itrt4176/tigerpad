3c50c5106c74d93313a9e1c13decac3c259b4efb
Nick Manos <nmanos@eastonrobotics.org>
2025-01-30 02:49:24 -0500

    Write out all inputs (except the rotary encoder)

--- code.py
+++ code.py
@@ -1,24 +1,55 @@
 import struct
-import time
 
+import board
 import usb_hid
 from adafruit_hid import find_device
+from supervisor import ticks_ms
 
-button_ids = range(1, 11)
+from constants import INPUT_REPORT_ID
+from hid_io.input import HIDAnalogInput, HIDDigitalInput
+
+digital_in_pins = (
+    board.GP0,
+    board.GP2,
+    board.GP4,
+    board.GP6,
+    board.GP8,
+    board.GP10,
+    board.GP12,
+    board.GP14,
+    board.GP16,
+    board.GP19,
+)
+digital_in_ids = range(1, 11)
+digital_hids = [
+    HIDDigitalInput(pin, hid_id) for pin, hid_id in zip(digital_in_pins, digital_in_ids)
+]
+
+analog_in_pins = (board.GP26_A0, board.GP27_A1, board.GP28_A2)
+analog_in_ids = range(1, 4)
+analog_hids = [
+    HIDAnalogInput(pin, hid_id) for pin, hid_id in zip(analog_in_pins, analog_in_ids)
+]
 
 device: usb_hid.Device = find_device(usb_hid.devices, usage_page=0x01, usage=0x05)  # type: ignore
 
+last_run = ticks_ms()
 while True:
-    report = bytearray(2)
-    for i in button_ids:
-        state_bits: int = 0
-        for j in button_ids:
-            state = i == j
-            state_bits |= state << (j - 1)
+    if ticks_ms() >= last_run + 5:
+        report = bytearray(10)
 
-        struct.pack_into("<H", report, 0, state_bits)
+        digital_bits: int = 0
+        for digital_in in digital_hids:
+            digital_bits |= digital_in.state << (digital_in.id - 1)
 
-        device.send_report(report, 1)
-        print(report)
+        struct.pack_into(
+            "<Hhhhh",
+            report,
+            0,
+            digital_bits,
+            *[analog_in.state for analog_in in analog_hids],
+            0,
+        )
 
-        time.sleep(1)
+        device.send_report(report, INPUT_REPORT_ID)
+        last_run = ticks_ms()
