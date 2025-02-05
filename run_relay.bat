9dfaeec14c3c7475a6c93e5bb0f10ed932515ecd
Nick Manos <nmanos@eastonrobotics.org>
2025-02-05 16:20:52 -0500

    Add batch script to manage relay process and update dependencies

---
+++ run_relay.bat
@@ -0,0 +1,30 @@
+@echo off
+TITLE TigerPad Relay
+SETCONSOLE /hide
+
+IF EXIST relay.pid (
+    GOTO stoprelay
+) else (
+    GOTO runupdate
+)
+
+:stoprelay
+FOR /F "tokens=*" %%p IN (relay.pid) DO taskkill /pid %%p
+
+:runupdate
+git pull
+
+IF EXIST .venv\ (
+    GOTO updatedeps
+) else (
+    GOTO createvenv
+)
+
+:createvenv
+py -m venv .venv
+
+:updatedeps
+pip install -r requirements.txt
+
+:startrelay
+py host_relay_client.py
