79a93dc0d088426891a4c6cbbbd1c5ccaa07a988
Nick Manos <nmanos@eastonrobotics.org>
2025-02-06 21:02:24 -0500

    Fix pipdeptree check and error formatting in build-requirements.sh

--- scripts/build-requirements.sh
+++ scripts/build-requirements.sh
@@ -2,15 +2,15 @@
 
 echoerr() { >&2 echo $@; }
 
-RED='\x1b[30m'
-BOLDRED='\x1b[90m'
-NC='\x1b[39m'
+RED='\x1b[0;31m'
+BOLDRED='\x1b[1;31m'
+NC='\x1b[0;39m'
 
 if ! command -v pipdeptree > /dev/null 2>&1
 then
-    echoerr "${BOLDRED}ERROR: ${RED}This script requires pipdeptree."
-    echoerr "You can install pipdeptree by running 'pipx install deptree'."
-    return 1
+    echoerr -e "${BOLDRED}ERROR: ${RED}This script requires pipdeptree.${NC}"
+    echoerr -e "Run 'pip install -r requirements-dev.txt' to resolve this error."
+    exit 1
 fi
 
-pipdeptree -f --python=auto --local-only --exclude pip | grep -E '^[a-zA-Z0-9\-]+' | tee requirements.txt
+pipdeptree -f --python=auto --local-only --exclude pip,pipdeptree | grep -E '^[a-zA-Z0-9\-]+' | tee requirements.txt
