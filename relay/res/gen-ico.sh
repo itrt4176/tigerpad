45d06696f64a1fccb9c49bee3851dd1042cd5707
Nick Manos <nmanos@eastonrobotics.org>
2025-02-18 22:26:50 -0500

    Move gen-ico.sh to scripts folder

--- relay/res/gen-ico.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-convert -density 256x256 -background transparent itrt_paw.svg -define icon:auto-resize -colors 256 tigerpad.ico
---
+++ scripts/gen-ico.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+convert -density 256x256 -background transparent relay/res/itrt_paw.svg -define icon:auto-resize -colors 256 relay/res/tigerpad.ico
