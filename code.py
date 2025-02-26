50fdb02ceecfb4edcb462fc88e3a80b6be244044
Nick Manos <nmanos@eastonrobotics.org>
2025-02-26 03:41:59 -0500

    Change time tracking to time.monotonic_ns()
    This fixes a bug due to ticks_ms() wrapping after about 65 seconds from boot, resulting in LEDs not blinking and USB_HID reports not being sent.

--- code.py
+++ code.py
@@ -1,16 +1,17 @@
 import struct
+import time
 
 import board
 import digitalio
 import microcontroller
 import usb_cdc
 import usb_hid
-from adafruit_hid import find_device # type: ignore
-from supervisor import ticks_ms
+from adafruit_hid import find_device  # type: ignore
 
-from constants import FW_UPDATE_PIN
+from constants import FW_UPDATE_PIN, REPORT_SIZE
 from hid_io.input import HIDAnalogInput, HIDDigitalInput, HIDRotaryEncoder
 from hid_io.output import HIDLEDOutput
+from utils import DEBUG_MODE
 
 fw_update_btn = digitalio.DigitalInOut(FW_UPDATE_PIN)
 fw_update_btn.pull = digitalio.Pull.DOWN
@@ -58,42 +59,68 @@ led_hids = [HIDLEDOutput(pin, hid_id) for pin, hid_id in zip(led_out_pins, led_o
 device: usb_hid.Device = find_device(usb_hid.devices, usage_page=0x01, usage=0x05)  # type: ignore
 
 send_errors = 0
-last_run = ticks_ms()
+last_run = 0
+last_digital = 0
+last_analog = [0] * len(analog_hids)
+last_rotary = 0
+
 while True:
     if fw_update_btn.value:
         microcontroller.reset()
 
-    if ticks_ms() >= last_run + 10:
-        report = bytearray(10)
+    if time.monotonic_ns() >= last_run + 10_000_000:
+        report = bytearray(REPORT_SIZE)
 
         digital_bits: int = 0
         for digital_in in digital_hids:
             digital_bits |= digital_in.state << (digital_in.id - 1)
-        
-        print(rotary_encoder.state)
 
-        struct.pack_into(
-            "<Hhhhh",
-            report,
-            0,
-            digital_bits,
-            *[analog_in.state for analog_in in analog_hids],
-            rotary_encoder.state,
+        rotary_state = rotary_encoder.state
+        analog_state = [analog_in.state for analog_in in analog_hids]
+
+        changed = (
+            last_digital != digital_bits
+            or last_rotary != rotary_state
+            or any(abs(new - old) > 41 for (old, new) in zip(last_analog, analog_state))
         )
 
-        try:
-            device.send_report(report)
-            last_run = ticks_ms()
-            if send_errors > 0:
-                print("Sent")
-            send_errors = 0
-        except OSError as e:
-            send_errors = send_errors + 1
-            print(f"send_report: {e} (error count {send_errors})")
+        if changed:
+            if DEBUG_MODE:
+                print(f"[{time.monotonic_ns()}] Report updated")
+
+            last_digital = digital_bits
+            last_analog = analog_state
+            last_rotary = rotary_state
+
+            struct.pack_into(
+                "<Hhhhh",
+                report,
+                0,
+                digital_bits,
+                *analog_state,
+                rotary_state,
+            )
+
+            try:
+                device.send_report(report)
+                last_run = time.monotonic_ns()
+                if send_errors > 0:
+                    if DEBUG_MODE:
+                        print("Sent")
+                send_errors = 0
+            except OSError as e:
+                send_errors = send_errors + 1
+                if DEBUG_MODE:
+                    print(f"send_report: {e} (error count {send_errors})")
 
     if serial.connected and serial.in_waiting >= 2:
+        if DEBUG_MODE:
+            print(f"[{time.monotonic_ns()}] Rcvd relay data")
+            print(f"serial waiting: {serial.in_waiting}")
         output_states = bytearray(2)
-        serial.readinto(output_states)
+
+        while serial.in_waiting > 0:
+            serial.readinto(output_states)
 
         led_bits: int = struct.unpack_from("<H", output_states)[0]
         for led in led_hids:
